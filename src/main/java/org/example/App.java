582beb92139f9635187202d75a5891fa4f177a83
BroJin <gudwls2777@naver.com>
2026-02-05 22:13:45 +0900

    feat: step-6

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner sc= new Scanner(System.in);
+    int lastId=0;
+    WiseSaying[] wiseSayings = new WiseSaying[10]; // 객체 한 개 생성은 배열에 안 들어감
+    int lastWiseSayingIndex = -1;
+
+    public void run(){
+        System.out.println("== 명언 앱 ==");
+
+        while(true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+            if (cmd.equals("종료")) {
+                System.exit(0);
+            } else if (cmd.equals("등록")) {
+                actionWrite();
+            } else if (cmd.equals("목록")){
+                actionList();
+            } else if (cmd.startsWith("삭제")){
+                actionDelete(cmd);
+            }
+        }
+    }
+
+    private void actionDelete(String cmd) {
+        String idStr = cmd.split("=")[1];
+        int id = Integer.parseInt(idStr);
+        delete(id);
+        System.out.println("1번 명언이 삭제되었습니다.");
+    }
+
+    private void delete(int deleteTarget) {
+        int foundIndex = -1;
+        for(int i=0; i<=lastWiseSayingIndex; i++){
+            WiseSaying foundedWiseSaying=wiseSayings[i];
+            if(deleteTarget==foundedWiseSaying.id){
+                foundIndex=i;
+            }
+        }
+
+        if(foundIndex == -1) return;
+
+        for(int i=foundIndex;i<lastWiseSayingIndex;i++){
+            wiseSayings[i]=wiseSayings[i+1];
+        }
+
+        lastWiseSayingIndex--; // 목록을 출력할 때
+    }
+
+
+    private void actionList() {
+        System.out.println("-----------------");
+        System.out.println("번호 / 작가 / 명언 ");
+
+        for(int i=lastWiseSayingIndex;i>=0;i--){
+            System.out.printf("%d / %s / %s\n", wiseSayings[i].id, wiseSayings[i].author,wiseSayings[i].content);
+        }
+    }
+
+    private void actionWrite() {
+        System.out.print("명언 : ");
+        String content = sc.nextLine();
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+
+        write(content, author);
+        System.out.printf("%d번 명언이 등록되었습니다.\n",lastId); //printf 사용
+    }
+
+    private void write(String content, String author){
+        WiseSaying wiseSaying = new WiseSaying();
+
+        wiseSaying.id = ++lastId;
+        wiseSaying.content=content;
+        wiseSaying.author=author;
+
+        wiseSayings[++lastWiseSayingIndex]=wiseSaying;
+
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("== 명언 앱 ==");
-        Scanner sc= new Scanner(System.in);
-        String cmd;
-        int lastId=0;
-
-        WiseSaying[] wiseSayings = new WiseSaying[10]; // 객체 한 개 생성은 배열에 안 들어감
-
-        while(true) {
-            System.out.print("명령) ");
-            cmd=sc.nextLine();
-            if (cmd.equals("종료")) {
-                System.exit(0);
-            } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
-                wiseSayings[lastId] = new WiseSaying(); // 객체를 먼저 생성 후 값을 넣어야 함.
-                wiseSayings[lastId].content = sc.nextLine(); // 생성 안하고 바로 값을 넣으면 없는 객체여서 오류 발생
-                System.out.print("작가 : ");
-                wiseSayings[lastId].author = sc.nextLine();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", ++lastId); //printf 사용
-
-            } else if (cmd.equals("목록")){
-                for(int i=lastId-1;i>=0;i--){
-                    System.out.printf("%d / %s / %s\n", i+1, wiseSayings[i].author,wiseSayings[i].content);
-                }
-            }
-
-        }
+        App app = new App();
+        app.run();
     }
 }
